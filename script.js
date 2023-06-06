943dd9e4891d93fdbbb8b85b26c0aee7f363f50a
Jam <128758652+Jampsant@users.noreply.github.com>
2023-06-06 17:12:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+function Yes(){alert("You acepted, great choice! :)")}function desvia(a){var b=a;b.style.position="absolute",b.style.bottom=geraPosicao(10,90),b.style.left=geraPosicao(10,90),console.log("nah...")}function geraPosicao(a,b){return Math.random()*(b-a)+a+"%"}
